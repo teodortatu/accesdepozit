76ebf9eb649434777065ac1fa55716f1671c7e1f
teodortatu <miatateo@yahoo.com>
2015-04-22 21:08:42 +0300

    I add index, add and rosii pages.

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+</head>
+<body>
+<div><h1>Login</h1>
+<form action="index.html" id="login_form" name="login_form">
+Username:
+<br>
+<input name="username" type="text" id="username" form="login_form" title="Username" autocomplete="on">
+<br>
+Password:
+<br>
+<input type="password" type="password" required id="password" title="Password">
+<input type="submit" value="Login">
+</form>
+</body>
+</html>
